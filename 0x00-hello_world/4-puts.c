72dbe11c33b8adede03e1c734e9c580c4afecc4b
2f91c8327fe6 <usmansuleiman5050@yahoo.com>
2023-02-20 07:59:16 -0800

    d

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
 #!/bin/bash
-/**this is the description
+#include <stdio.h>
+/**
+*this is the description
 */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
+
