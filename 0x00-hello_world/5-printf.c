0e029f2f0f423fe9f78459106adc338f20304790
2f91c8327fe6 <usmansuleiman5050@yahoo.com>
2023-02-21 02:54:23 -0800

    s

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
-*another program
-*/
+ * another program
+ */
 
 int main(void)
 {
-printf("with proper gramer. but the outcome is a piece of art, 
+printf("with proper gramer, but the outcome is a piece of art, 
 ");
 return(0);
 }
